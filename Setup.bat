6caecb27658aa79890c19a066275ba907ea8e528
Nguyen Dong Kha <72005690+ndkcuber@users.noreply.github.com>
2022-01-07 10:42:26 +0700

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,17 @@
+title Setting up... Please wait
+pip3 install requests urllib3
+pip install requests
+pip install PySocks
+pip install threading
+pip install random
+pip install re
+pip install os
+pip install ssl
+pip install webbrowser
+pip install bz2
+pip install datetime
+pip install wget
+pip install json
+pip install cfscrape
+pip install glob
+pip install time
